8353d7756fdbc2f4e037acc839309d32edd7ebf2
Bogdan Craciun <bogdan@amn.nl>
2023-12-07 20:33:34 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -2,5 +2,12 @@ echo "\r\n Installing BREW package manager... \r\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Add this to .zshrc:
-# eval $(/opt/homebrew/bin/brew shellenv)
+# After the install is done, run this:
+# (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/bogdan/.zprofile
+# And this:
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Brew stuff to install:
+# brew install gh
+# brew install --cask marta
+# 
