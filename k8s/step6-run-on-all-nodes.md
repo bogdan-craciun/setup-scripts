9bbf0df7d7a8dc37b6d302a5f5c597d6b1320a8c
Bogdan Craciun <bogdan@amn.nl>
2024-09-17 16:01:33 +0300

    cleanup

--- k8s/step6-run-on-all-nodes.md
+++
@@ -1,8 +0,0 @@
-- Configure containerd:
-
-```bash
-sudo nano /etc/containerd/config.toml
-```
-
-Then, after the line `[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]` find
-and make sure `SystemdCgroup` is set to `true`, like so: `SystemdCgroup = true`.
--- k8s/step7-run-on-all-nodes.sh
+++
@@ -1,9 +0,0 @@
-# Restart containerd to apply changes and pull in base images:
-
-sudo systemctl restart containerd
-sudo systemctl enable containerd
-sudo systemctl enable kubelet
-
-sudo kubeadm config images pull
-
-sudo kubeadm config images pull --cri-socket /run/containerd/containerd.sock
